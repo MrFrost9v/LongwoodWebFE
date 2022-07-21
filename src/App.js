90a317e82535d123e057381a42cbce6170b254c5
Augustin Espinosa <aespinosa@longwoodgardens.org>
2022-07-21 14:05:47 -0400

    Made it possible to make a call to the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Percentages: {
+        
+          Location: "",
+          People: 0,
+          Clients: 0,
+          Percentage: 0
+        
+    }
+  }
 }
 
+  async componentDidMount() {
+  fetch("https://localhost:7039/Percentages")
+    .then(res => {
+      res.json().then(Percentages => this.setState({ Percentages }));
+    })
+  }
+
+  render() {
+    while (this.state.Percentages == null)
+    {
+      return "..."
+    } 
+    const Data = this.state.Percentages.Clients.toString();
+
+  return (
+      <div className="App">
+        <div className='Percentages-Header'>
+          <p>
+            LongWood Gardens Visitors to Clients Ratios
+          </p>
+        </div>
+        <div className='Percentages'>
+          <p>
+            {Data}
+            </p>
+        </div>
+
+      </div>
+    );
+  }
+}
+//{this.Conservatory()}
 export default App;
